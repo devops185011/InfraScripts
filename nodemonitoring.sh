c9d4ff17beeb61d6c452f596689d7de15769b985
kanchan upadhyay <kanchan.upadhyay@spglobal.com>
2024-11-16 05:57:37 +0530

    abc

--- nodemonitoring.sh
+++ nodemonitoring.sh
@@ -1,6 +1,8 @@
 
 #!/bin/bash
-set -x
+set -x # debug mode
+set -e  # exit the script if there is an error, doesn't exit if there are any pipe failures, that is why we are using below command
+set -o pipefail #
 echo "print the disk space"
 du -h 
 
